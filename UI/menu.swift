107b5baf4e62d4356b05d984b91f184448d7316b
Ethan Chen <ethanchen006@gmail.com>
2024-01-25 22:14:49 -0800

    test

---
+++ UI/menu.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct MainMenu: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
