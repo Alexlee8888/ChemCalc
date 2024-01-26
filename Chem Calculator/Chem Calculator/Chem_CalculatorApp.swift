74d8bb67b6e4162b635f1ae552c3e5d09589eb8f
Ethan Chen <ethanchen006@gmail.com>
2024-01-25 22:39:12 -0800

    a

---
+++ Chem Calculator/Chem Calculator/Chem_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chem_CalculatorApp.swift
+//  Chem Calculator
+//
+//  Created by Ethan Chen on 1/25/24.
+//
+
+import SwiftUI
+
+@main
+struct Chem_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Chem Calculator/Chem Calculator/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Chem Calculator
+//
+//  Created by Ethan Chen on 1/25/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
